70cad7fa2a45247d92518c3dad8bee0c3a296b0b
MehaletKB <mkesatebirhan@gmail.com>
2021-06-15 12:58:11 -0500

    index and style setup

---
+++ Assets/JS/script.js
@@ -0,0 +1,6 @@
+const api = {
+    key: "7055b12a6623e70c9988cf2e89bf1f83";
+    BASE_URL: "https://api.openweathermap.org/data/2.5/onecall";
+}
+
+const search = document.querySelector('search-tex')
